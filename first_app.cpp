a2f33aaf1b41206141ba69e0130660b4f8df2b67
stairface <pablo.jofre34@gmail.com>
2021-11-03 23:33:32 +0100

    refactoring rendering

--- first_app.cpp
+++ first_app.cpp
@@ -20,8 +20,7 @@ namespace vt {
     FirstApp::FirstApp() {
         loadGameObjects();
         createPipelineLayout();
-        recreateSwapChain();
-        createCommandBuffers();
+        createPipeline();
     }
 
     FirstApp::~FirstApp() {
@@ -31,7 +30,13 @@ namespace vt {
     void FirstApp::run() {
         while(!vtWindow.shouldClose()) {
             glfwPollEvents();
-            drawFrame();
+
+            if(auto commandBuffer = vtRenderer.beginFrame()) {
+                vtRenderer.beginSwapChainRenderPass(commandBuffer);
+                renderGameObjects(commandBuffer);
+                vtRenderer.endSwapChainRenderPass(commandBuffer);
+                vtRenderer.endFrame();
+            }
         }
 
         vkDeviceWaitIdle(vtDevice.device());
@@ -76,109 +81,17 @@ namespace vt {
     }
 
     void FirstApp::createPipeline() {
-        assert(vtSwapChain != nullptr && "cannor create pipeline before swapchain");
         assert(pipelineLayout != nullptr && "cannot create pipeline before pipeline layout");
 
         PipelineConfigInfo pipelineConfig{};
         VtPipeline::defaultPipelineConfigInfo(pipelineConfig);
-        pipelineConfig.renderPass = vtSwapChain->getRenderPass();
+        pipelineConfig.renderPass = vtRenderer.getSwapChainRenderPass();
         pipelineConfig.pipelineLayout = pipelineLayout;
         vtPipeline = std::make_unique<VtPipeline>(
             "shaders/simple_shader.vert.spv",
             "shaders/simple_shader.frag.spv",
             vtDevice,
             pipelineConfig);
-    }
-
-    void FirstApp::recreateSwapChain() {
-        auto extent = vtWindow.getExtent();
-
-        while(extent.width == 0 || extent.height == 0) {
-            extent = vtWindow.getExtent();
-            glfwWaitEvents();
-        }
-
-        vkDeviceWaitIdle(vtDevice.device());
-
-        if(vtSwapChain == nullptr) {
-            vtSwapChain = std::make_unique<VtSwapChain>(vtDevice, extent);
-        } else {
-            vtSwapChain = std::make_unique<VtSwapChain>(vtDevice, extent, std::move(vtSwapChain));
-
-            if(vtSwapChain->imageCount() != commandBuffers.size()) {
-                freeCommandBuffers();
-                createCommandBuffers();
-            }
-        }
-        createPipeline();
-    }
-
-    void FirstApp::createCommandBuffers() {
-        commandBuffers.resize(vtSwapChain->imageCount());
-        VkCommandBufferAllocateInfo allocInfo{};
-        allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
-        allocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
-        allocInfo.commandPool = vtDevice.getCommandPool();
-        allocInfo.commandBufferCount = static_cast<uint32_t>(commandBuffers.size());
-
-        if(vkAllocateCommandBuffers(vtDevice.device(), &allocInfo, commandBuffers.data()) != VK_SUCCESS) {
-            throw std::runtime_error("failed to allocate command buffers");
-        }
-    }
-
-    void FirstApp::freeCommandBuffers() {
-        vkFreeCommandBuffers(
-            vtDevice.device(),
-            vtDevice.getCommandPool(),
-            static_cast<uint32_t>(commandBuffers.size()),
-            commandBuffers.data()
-            );
-        commandBuffers.clear();
-    }
-    
-    void FirstApp::recordCommandBuffer(int imageIndex) {
-
-        VkCommandBufferBeginInfo beginInfo{};
-        beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
-
-        if(vkBeginCommandBuffer(commandBuffers[imageIndex], &beginInfo) != VK_SUCCESS) {
-            throw std::runtime_error("failed to begin recording command buffer");
-        }
-
-        VkRenderPassBeginInfo renderPassInfo{};
-        renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
-        renderPassInfo.renderPass = vtSwapChain->getRenderPass();
-        renderPassInfo.framebuffer = vtSwapChain->getFrameBuffer(imageIndex);
-
-        renderPassInfo.renderArea.offset = {0, 0};
-        renderPassInfo.renderArea.extent = vtSwapChain->getSwapChainExtent();
-
-        std::array<VkClearValue, 2> clearValues{};
-        clearValues[0].color = {0.01f, 0.01f, 0.01f, 1.0f};
-        clearValues[1].depthStencil = {1.0f, 0};
-        renderPassInfo.clearValueCount = static_cast<uint32_t>(clearValues.size());
-        renderPassInfo.pClearValues = clearValues.data();
-
-        vkCmdBeginRenderPass(commandBuffers[imageIndex], &renderPassInfo, VK_SUBPASS_CONTENTS_INLINE);
-
-        VkViewport viewport{};
-        viewport.x = 0.0f;
-        viewport.y = 0.0f;
-        viewport.width = static_cast<float>(vtSwapChain->getSwapChainExtent().width);
-        viewport.height = static_cast<float>(vtSwapChain->getSwapChainExtent().height);
-        viewport.minDepth = 0.0f;
-        viewport.maxDepth = 1.0f;
-        VkRect2D scissor{{0, 0}, vtSwapChain->getSwapChainExtent()};
-        vkCmdSetViewport(commandBuffers[imageIndex], 0, 1, &viewport);
-        vkCmdSetScissor(commandBuffers[imageIndex], 0, 1, &scissor);
-
-        renderGameObjects(commandBuffers[imageIndex]);
-        
-        vkCmdEndRenderPass(commandBuffers[imageIndex]);
-
-        if(vkEndCommandBuffer(commandBuffers[imageIndex]) != VK_SUCCESS) {
-            throw std::runtime_error("failed to record command buffer");
-        }
     }
 
     void FirstApp::renderGameObjects(VkCommandBuffer commandBuffer) {
@@ -208,32 +121,6 @@ namespace vt {
 
             obj.model->bind(commandBuffer);
             obj.model->draw(commandBuffer);
-        }
-    }
-
-    void FirstApp::drawFrame() {
-        uint32_t imageIndex;
-        auto result = vtSwapChain->acquireNextImage(&imageIndex);
-
-        if(result == VK_ERROR_OUT_OF_DATE_KHR) {
-            recreateSwapChain();
-        }
-
-        if(result != VK_SUCCESS && result != VK_SUBOPTIMAL_KHR) {
-            throw std::runtime_error("failed to acuire swapchain image");
-        }
-
-        recordCommandBuffer(imageIndex);
-        result = vtSwapChain->submitCommandBuffers(&commandBuffers[imageIndex], &imageIndex);
-
-        if(result == VK_ERROR_OUT_OF_DATE_KHR || result == VK_SUBOPTIMAL_KHR || vtWindow.wasWindowResized()) {
-            vtWindow.resetWindowResizeFlag();
-            recreateSwapChain();
-            return;
-        }
-
-        if(result != VK_SUCCESS) {
-            throw std::runtime_error("failed to present swapchain image");
         }
     }
 }
---
+++ vt_renderer.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "vt_window.hpp"
+#include "vt_device.hpp"
+#include "vt_swap_chain.hpp"
+
+#include <cassert>
+#include <memory>
+#include <vector>
+
+namespace vt {
+    class VtRenderer {
+        public:
+
+            VtRenderer(VtWindow &window, VtDevice &device);
+            ~VtRenderer();
+
+            VtRenderer(const VtRenderer &) = delete;
+            VtRenderer &operator=(const VtRenderer &) = delete;
+
+            VkRenderPass getSwapChainRenderPass() const { return vtSwapChain->getRenderPass(); }
+
+            bool isFrameInProgress() { return isFrameStarted; }
+
+            VkCommandBuffer getCurrentCommandBuffer() const {
+                assert(isFrameStarted && "cannot get command buffer when frame not in progress");
+                return commandBuffers[currentImageIndex];
+            }
+
+            VkCommandBuffer beginFrame();
+            void endFrame();
+            void beginSwapChainRenderPass(VkCommandBuffer commandBuffer);
+            void endSwapChainRenderPass(VkCommandBuffer commandBuffer);
+
+        private:
+            void createCommandBuffers();
+            void freeCommandBuffers();
+            void recreateSwapChain();
+
+            VtWindow& vtWindow;
+            VtDevice& vtDevice;
+            std::unique_ptr<VtSwapChain> vtSwapChain;
+            std::vector<VkCommandBuffer> commandBuffers;
+
+            uint32_t currentImageIndex;
+            bool isFrameStarted = false;
+    };
+}
