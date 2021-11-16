4a03c07012f5b7fe7d0c437b836f178077f48d44
stairface <pablo.jofre34@gmail.com>
2021-11-16 14:26:51 +0100

    created index buffer

--- vt_model.hpp
+++ vt_model.hpp
@@ -20,7 +20,12 @@ namespace vt {
                 static std::vector<VkVertexInputAttributeDescription> getAttributeDescriptions();
             };
 
-            VtModel(VtDevice &device, const std::vector<Vertex> &vertices);
+            struct Builder {
+                std::vector<Vertex> vertices{};
+                std::vector<uint32_t> indices{};
+            };
+
+            VtModel(VtDevice &device, const VtModel::Builder &builder);
             ~VtModel();
 
             VtModel(const VtModel &) = delete;
@@ -30,11 +35,18 @@ namespace vt {
             void draw(VkCommandBuffer commandBuffer);
         private:
             void createVertexBuffers(const std::vector<Vertex> &vertices);
+            void createIndexBuffers(const std::vector<uint32_t> &indices);
 
             VtDevice& vtDevice;
+
             VkBuffer vertexBuffer;
             VkDeviceMemory vertexBufferMemory;
             uint32_t vertexCount;
+
+            bool hasIndexBuffer = false;
+            VkBuffer indexBuffer;
+            VkDeviceMemory indexBufferMemory;
+            uint32_t indexCount;
 
     };
 }
